821c026446480c2d04dc67cad8d2542cd10c45b6
moogi <moogimoogi0101@gmail.com>
2022-11-21 11:33:11 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var scores = [0, 0];
 // -----------ээлжиндээ цуглуулж байгаа оноог хадгалах
 var roundScore = 0;
 // --------шооны аль талаараа буусанг 1-6 санамсаргүй
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
 // <div class="player-score" id="score-0">43</div>
 // window.document.querySelector("#score-0").textContent = dice;
@@ -13,12 +13,20 @@ var dice = Math.floor(Math.random() * 6) + 1;
 // document.querySelector("#score-1").textContent = dice;
 
 // --------------------Программ эхлэхэд бэлдэх
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
 //-------------------------------------------------//
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-console.log("Шоо : " + dice);
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+
+  //   alert("Шоо буулаа: " + diceNumber);
+});
